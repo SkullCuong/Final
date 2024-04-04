6d6e5b036fa2a87bc2c35c03af13e57918c994ab
unknown <jackscuong@gmail.com>
2024-04-04 12:52:31 +0700

    CRUD Room

--- controllers/Room.js
+++ controllers/Room.js
@@ -20,18 +20,27 @@ class Room {
     this.capacity = capacity;
     this.delete_status = delete_status;
   }
+  static async index(req, res) {
+    const roomDb = await db.Room.findAll();
+    const rooms = roomDb.map((room) => room.get({ plain: true }));
+    res.render("Room/room", { room: rooms });
+  }
+
   static createRender(req, res) {
     res.render("Room/CreateRoom");
   }
+
   static async create(req, res) {
     const { name, type, floor, price, capacity } = req.body;
     const room = new Room(name, type, floor, price, capacity);
     try {
       await db.Room.create(room);
+      res.redirect("/room");
     } catch (err) {
       console.log(err);
     }
   }
+
   static async updateRender(req, res) {
     const id = req.params.id;
     try {
@@ -44,6 +53,7 @@ class Room {
       console.log(err);
     }
   }
+
   static async update(req, res) {
     const id = req.params.id;
     const { name, type, floor, price, capacity } = req.body;
@@ -52,6 +62,18 @@ class Room {
         { name, type, floor, price, capacity },
         { where: { id: id } }
       );
+      res.redirect("/room");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async delete(req, res) {
+    const id = req.params.id;
+    const delete_status = true;
+    try {
+      await db.Room.update({ delete_status }, { where: { id: id } });
+      res.redirect("/room");
     } catch (err) {
       console.log(err);
     }
